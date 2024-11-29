6e72301fbb17a1b639dfa1ed7d82ba218a7462d0
Roob!e <80145015+roob1e@users.noreply.github.com>
2024-11-29 13:34:20 +0300

    modified:   frontend/main.py

--- frontend/main.py
+++ frontend/main.py
@@ -1,5 +1,5 @@
 import flet as ft
-import os
+import os, sys, subprocess
 
 from assets.objects import objects_main
 from assets.objects import objects_contacts
@@ -43,190 +43,200 @@ def main(page: ft.Page):
         #page.adaptive = ft.Auto
         page.padding = None
         
+        page.overlay.append(objects_main.lgn_popup)
+        page.overlay.append(objects_main.reg_popup)
         if os.path.exists("frontend/assets/session.whz"):
             objects_main.profile_btn.content = ft.Text(tokens.read_login(), size = 20)
             page.appbar = objects_main.appbar_logged
+            page.overlay.append(objects_main.post_popup)
+            def on_hover_1(event: ft.HoverEvent):
+                if event.data == "true":
+                    objects_main.liked.scale = 1.03
+                    page.window.on_event
+                else: 
+                    objects_main.liked.scale = 1.0
+                page.update()
+            
+            def on_hover_2(event: ft.HoverEvent):
+                if event.data == "true":
+                    objects_main.conf.scale = 1.03
+                else:
+                    objects_main.conf.scale = 1.0
+                page.update()
+                
+            def on_hover_3(event: ft.HoverEvent):
+                if event.data == "true":
+                    objects_main.online.scale = 1.03
+                else:
+                    objects_main.online.scale = 1.0
+                page.update()
+                
+            def open_writer(e):
+                objects_main.post_popup.open = True
+                page.update()
+                
+            def close_writer(e):
+                objects_main.post_popup.open = False
+                page.update()
+                            
+            def write_post(*args):
+                title = objects_main.post_header_field.value
+                topic = objects_main.theme_picker.value
+                text = objects_main.post_text_field.value
+                
+                print(topic, title, text)
+                
+                response = create_theme(topic, title, text)
+                print(f"--------------{response}")
+                
+                if response[0] == 200:
+                    alert = ft.AlertDialog(content = ft.Container(content = ft.Text("Ваша тема была опубликована", size = 20, width = 360, text_align = ft.TextAlign.CENTER, height = 25), alignment = ft.alignment.center, height = 30), open = False, bgcolor = "#1C1C1C")
+                else:
+                    alert = ft.AlertDialog(content = ft.Text("Ошибка публикации!", size = 20, width = 360), open = False, bgcolor = "#1C1C1C")
+                page.overlay.append(alert)
+                alert.open = True
+                page.update()
+            
+            def open_profile_popup(*args):
+                objects_main.username.value = tokens.read_login()
+                page.overlay.append(objects_main.profile_popup)
+                objects_main.profile_popup.open = True
+                page.update()
+            
+            objects_main.liked.on_hover = on_hover_1
+            objects_main.conf.on_hover = on_hover_2
+            objects_main.online.on_hover = on_hover_3
+            
+            objects_main.contacts.on_click = contacts
+            objects_main.home.on_click = main_page
+            objects_main.rules.on_click = rules
+            objects_main.about.on_click = about_us
+            objects_main.help_.on_click = help
+            objects_main.posts.on_click = open_writer
+            
+            objects_main.cancel_post.on_click = close_writer
+            
+            objects_main.send_post.on_click = write_post
+            objects_main.profile_btn.on_click = open_profile_popup
+            
+            categories_container = ft.Container(
+                content = objects_main.categories_column,
+                width = 300,
+                bgcolor = "#151515",
+                padding = ft.padding.all(50),
+                margin = ft.margin.only(right = 50),
+                border_radius = 20
+            )
+            
+            main_container = ft.Container(
+                content = ft.Column(
+                    [
+                        objects_main.main_column,
+                        threads_container.get_main_threads(page)
+                    ], scroll = ft.ScrollMode.ADAPTIVE,
+                    spacing = 50  
+                ),
+                margin = ft.margin.only(right = 50),
+                height = 930,
+                padding = ft.padding.only(top = 30)
+            )
+            final_container = ft.Container(
+                content = ft.Row(
+                    [
+                        categories_container, 
+                        main_container
+                    ]
+                ),
+                padding = ft.padding.only(left = 100),
+                #alignment = ft.alignment.top_center
+            )
+            
+            page.add(final_container)
+            
+            
+            
         else:
             page.appbar = objects_main.appbar_unlogged
-        
-        page.update()
-        
-        def on_hover_1(event: ft.HoverEvent):
-            if event.data == "true":
-                objects_main.liked.scale = 1.03
-                page.window.on_event
-            else: 
-                objects_main.liked.scale = 1.0
-            page.update()
-        
-        def on_hover_2(event: ft.HoverEvent):
-            if event.data == "true":
-                objects_main.conf.scale = 1.03
-            else:
-                objects_main.conf.scale = 1.0
-            page.update()
             
-        def on_hover_3(event: ft.HoverEvent):
-            if event.data == "true":
-                objects_main.online.scale = 1.03
-            else:
-                objects_main.online.scale = 1.0
-            page.update()
-        
-        def open_lgn(e):
-            objects_main.lgn_popup.open = True
-            page.update()
-            
-        def open_reg(e):
-            objects_main.reg_popup.open = True
-            page.update()
-            
-        def open_writer(e):
-            objects_main.post_popup.open = True
-            page.update()
-            
-        def close_writer(e):
-            objects_main.post_popup.open = False
-            page.update()
-        
-        def redirect_to_reg(e):
-            objects_main.lgn_popup.open = False
-            objects_main.reg_popup.open = True
-            page.update()
-            
-        def redirect_to_lgn(e):
-            objects_main.reg_popup.open = False
-            objects_main.lgn_popup.open = True
-            page.update()
-            
-        def login(*args):
-            login = objects_main.lgn_login_field.value
-            password = objects_main.lgn_pass_field.value
-            
-            response = auth.login(login, password)
-            
-            if response == 200:
-                tokens.write_login(objects_main.lgn_login_field.value) 
-                objects_main.lgn_login_field.value = ""
-                objects_main.lgn_pass_field.value = ""
-                page.appbar = objects_main.appbar_logged
-                objects_main.profile_btn.text = tokens.read_login()
-                alert = ft.AlertDialog(content = ft.Text("Вы успешно вошли в учётную запись", size = 20, width = 360), open = False, bgcolor = "#1C1C1C")   
-            elif response == 400:
-                objects_main.lgn_login_field.value = ""
-                objects_main.lgn_pass_field.value = ""
-                alert = ft.AlertDialog(content = ft.Text("Неверный логин или пароль", size = 20, width = 300), open = False, bgcolor = "#1C1C1C")
+            def open_lgn(e):
+                objects_main.lgn_popup.open = True
+                page.update()
+                
+            def open_reg(e):
+                objects_main.reg_popup.open = True
+                page.update()
+                
+            def redirect_to_reg(e):
+                objects_main.lgn_popup.open = False
+                objects_main.reg_popup.open = True
+                page.update()
+                
+            def redirect_to_lgn(e):
+                objects_main.reg_popup.open = False
+                objects_main.lgn_popup.open = True
+                page.update()
+                
+            def login(*args):
+                login = objects_main.lgn_login_field.value
+                password = objects_main.lgn_pass_field.value
+                
+                response = auth.login(login, password)
+                
+                if response == 200:
+                    tokens.write_login(objects_main.lgn_login_field.value) 
+                    objects_main.lgn_login_field.value = ""
+                    objects_main.lgn_pass_field.value = ""
+                    page.appbar = objects_main.appbar_logged
+                    objects_main.profile_btn.text = tokens.read_login()
+                    alert = ft.AlertDialog(content = ft.Text("Вы успешно вошли в учётную запись", size = 20, width = 360), open = False, bgcolor = "#1C1C1C")   
+                    page.window.destroy()
+                    subprocess.Popen([sys.executable] + sys.argv)
+                    
+                elif response == 400:
+                    objects_main.lgn_login_field.value = ""
+                    objects_main.lgn_pass_field.value = ""
+                    alert = ft.AlertDialog(content = ft.Text("Неверный логин или пароль", size = 20, width = 300), open = False, bgcolor = "#1C1C1C")
 
-            objects_main.lgn_popup.open = False
-            page.overlay.append(alert)
-            alert.open = True
-            page.update()
+                objects_main.lgn_popup.open = False
+                page.overlay.append(alert)
+                alert.open = True
+                page.update()
+                    
+            def register(*args):
+                login = objects_main.reg_login_field.value
+                password = objects_main.reg_pass_field.value
                 
-        def register(*args):
-            login = objects_main.reg_login_field.value
-            password = objects_main.reg_pass_field.value
-            
-            response = auth.register(login, password)
-            
-            if response == 200:
-                objects_main.reg_login_field.value = ""
-                objects_main.reg_pass_field.value = ""
-                page.appbar = objects_main.appbar_logged
-                objects_main.profile_btn.text = tokens.read_login()
+                response = auth.register(login, password)
                 
-                alert = ft.AlertDialog(content = ft.Text("Вы успешно зарегистрировали учётную запись", size = 20, width = 360), open = False, bgcolor = "#1C1C1C")
-            elif response == 400:
-                objects_main.reg_login_field.value = ""
-                objects_main.reg_pass_field.value = ""
-                alert = ft.AlertDialog(content = ft.Text("Такой логин уже существует", size = 20, width = 300), open = False, bgcolor = "#1C1C1C")
-            
-            objects_main.reg_popup.open = False
-            page.overlay.append(alert)
-            alert.open = True
-            page.update()
-                          
-        def write_post(*args):
-            title = objects_main.post_header_field.value
-            topic = objects_main.theme_picker.value
-            text = objects_main.post_text_field.value
-            
-            print(topic, title, text)
-            
-            response = create_theme(topic, title, text)
-            print(f"--------------{response}")
-            
-            if response[0] == 200:
-                alert = ft.AlertDialog(content = ft.Container(content = ft.Text("Ваша тема была опубликована", size = 20, width = 360, text_align = ft.TextAlign.CENTER, height = 25), alignment = ft.alignment.center, height = 30), open = False, bgcolor = "#1C1C1C")
-            else:
-                alert = ft.AlertDialog(content = ft.Text("Ошибка публикации!", size = 20, width = 360), open = False, bgcolor = "#1C1C1C")
-            page.overlay.append(alert)
-            alert.open = True
-            page.update()
+                if response == 200:
+                    objects_main.reg_login_field.value = ""
+                    objects_main.reg_pass_field.value = ""
+                    page.appbar = objects_main.appbar_logged
+                    objects_main.profile_btn.text = tokens.read_login()
+                    
+                    alert = ft.AlertDialog(content = ft.Text("Вы успешно зарегистрировали учётную запись", size = 20, width = 360), open = False, bgcolor = "#1C1C1C")
+                    page.window.destroy()
+                    subprocess.Popen([sys.executable] + sys.argv)
+                    
+                elif response == 400:
+                    objects_main.reg_login_field.value = ""
+                    objects_main.reg_pass_field.value = ""
+                    alert = ft.AlertDialog(content = ft.Text("Такой логин уже существует", size = 20, width = 300), open = False, bgcolor = "#1C1C1C")
+                
+                objects_main.reg_popup.open = False
+                page.overlay.append(alert)
+                alert.open = True
+                page.update()
+                
+            objects_main.login.on_click = open_lgn
+            objects_main.register.on_click = open_reg
+            objects_main.lgn_redirect.on_click = redirect_to_reg
+            objects_main.reg_redirect.on_click = redirect_to_lgn
+            objects_main.login_btn.on_click = login
+            objects_main.reg_btn.on_click = register
         
-        def open_profile_popup(*args):
-            objects_main.username.value = tokens.read_login()
-            page.overlay.append(objects_main.profile_popup)
-            objects_main.profile_popup.open = True
-            page.update()
-        
-        objects_main.liked.on_hover = on_hover_1
-        objects_main.conf.on_hover = on_hover_2
-        objects_main.online.on_hover = on_hover_3
-        
-        objects_main.login.on_click = open_lgn
-        objects_main.register.on_click = open_reg
-        objects_main.contacts.on_click = contacts
-        objects_main.home.on_click = main_page
-        objects_main.rules.on_click = rules
-        objects_main.about.on_click = about_us
-        objects_main.help_.on_click = help
-        objects_main.posts.on_click = open_writer
-        
-        objects_main.lgn_redirect.on_click = redirect_to_reg
-        objects_main.reg_redirect.on_click = redirect_to_lgn
-        objects_main.cancel_post.on_click = close_writer
-        
-        objects_main.login_btn.on_click = login
-        objects_main.reg_btn.on_click = register
-        objects_main.send_post.on_click = write_post
-        objects_main.profile_btn.on_click = open_profile_popup
-        
-        categories_container = ft.Container(
-            content = objects_main.categories_column,
-            width = 300,
-            bgcolor = "#151515",
-            padding = ft.padding.all(50),
-            margin = ft.margin.only(right = 50),
-            border_radius = 20
-        )
-        
-        main_container = ft.Container(
-            content = ft.Column(
-                [
-                    objects_main.main_column,
-                    threads_container.get_main_threads(page)
-                ], scroll = ft.ScrollMode.ADAPTIVE,
-                spacing = 50  
-            ),
-            margin = ft.margin.only(right = 50),
-            height = 930,
-            padding = ft.padding.only(top = 30)
-        )
-        final_container = ft.Container(
-            content = ft.Row(
-                [
-                    categories_container, 
-                    main_container
-                ]
-            ),
-            padding = ft.padding.only(left = 100),
-            #alignment = ft.alignment.top_center
-        )
-        
-        page.overlay.append(objects_main.lgn_popup)
         page.overlay.append(objects_main.reg_popup)
-        page.overlay.append(objects_main.post_popup)
-        page.add(final_container)
+        page.overlay.append(objects_main.lgn_popup)
         page.update()
     
     def contacts(*args):
